c0fdc33835c31c5cb0bd2ba54c892ac419c9450c
Jistal <xhibril.dev@gmail.com>
2025-12-29 19:29:04 +0100

    fix

--- src/main/java/com/sipsync/sipsync/controller/EmergencyDbFixController.java
+++ src/main/java/com/sipsync/sipsync/controller/EmergencyDbFixController.java
@@ -1,8 +1,8 @@
 package com.sipsync.sipsync.controller;
 
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +12,7 @@ public class EmergencyDbFixController {
     @PersistenceContext
     private EntityManager em;
 
+    @Transactional
     @GetMapping("/__drop_users")
     public String dropUsersTable() {
         em.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
